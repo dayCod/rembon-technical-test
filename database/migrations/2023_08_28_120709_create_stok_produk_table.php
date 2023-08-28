6d820fdf74b5fa32dc2da726e2973e14132b4385
dayCod <alayarahman07@gmail.com>
2023-08-28 19:09:20 +0700

    Add: Table Migration

--- database/migrations/2023_08_28_120709_create_stok_produk_table.php
+++ database/migrations/2023_08_28_120709_create_stok_produk_table.php
@@ -11,15 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stok_produk', function (Blueprint $table) {
             $table->id();
-            $table->uuid();
-            $table->string('nama_depan', 30);
-            $table->string('nama_belakang', 30);
-            $table->string('alamat', 200);
-            $table->string('nomor_hp', 15);
-            $table->enum('jenis_kelamin', ['pria', 'wanita']);
-            $table->date('tgl_lahir');
             $table->timestamps();
         });
     }
@@ -29,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stok_produk');
     }
 };
