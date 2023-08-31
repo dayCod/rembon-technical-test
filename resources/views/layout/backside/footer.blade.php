630627cea2b94d767d114fa9b5e884b736eda889
dayCod <alayarahman07@gmail.com>
2023-08-31 15:14:51 +0700

    Fix: Stock Validation

--- resources/views/layout/backside/footer.blade.php
+++ resources/views/layout/backside/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer text-center text-muted">
-    All Rights Reserved by KUIKUIZ, Developed by <a
+    All Rights Reserved by AZ Products, Developed by <a
         href="https://github.com/dayCod" target="__blank">DayCod</a>.
 </footer>
